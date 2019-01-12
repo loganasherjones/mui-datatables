0a0dbcec8aad750c2f175f4baf936b2ddeb8947c
Logan Jones <loganasherjones@gmail.com>
2019-01-12 17:11:32 -0500

    Adding example using the filter column headers.

--- examples/themes/index.js
+++ examples/themes/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import MUIDataTable from '../../src/';
-import { MuiThemeProvider, createMuiTheme } from '@material-ui/core';
+import DATA from './data';
+import { Switch, MuiThemeProvider, createMuiTheme, Paper, Typography } from '@material-ui/core';
 
 class Example extends React.Component {
   render() {
@@ -10,8 +11,9 @@ class Example extends React.Component {
         name: 'Name',
         options: {
           filter: true,
-          filterList: ['Franky Miles'],
+          filterList: [],
           filterOptions: ['a', 'b', 'c', 'Business Analyst'],
+          hint: 'Some hint here',
         },
       },
       {
@@ -37,64 +39,75 @@ class Example extends React.Component {
         options: {
           filter: true,
           sort: false,
+          hint: 'Hint with no filter',
         },
       },
     ];
-    const data = [
-      ['Gabby George', 'Business Analyst', 'Minneapolis', 30, 100000],
-      ['Business Analyst', 'Business Consultant', 'Dallas', 55, 200000],
-      ['Jaden Collins', 'Attorney', 'Santa Ana', 27, 500000],
-      ['Franky Rees', 'Business Analyst', 'St. Petersburg', 22, 50000],
-      ['Aaren Rose', 'Business Consultant', 'Toledo', 28, 75000],
-      ['Blake Duncan', 'Business Management Analyst', 'San Diego', 65, 94000],
-      ['Frankie Parry', 'Agency Legal Counsel', 'Jacksonville', 71, 210000],
-      ['Lane Wilson', 'Commercial Specialist', 'Omaha', 19, 65000],
-      ['Robin Duncan', 'Business Analyst', 'Los Angeles', 20, 77000],
-      ['Mel Brooks', 'Business Consultant', 'Oklahoma City', 37, 135000],
-      ['Harper White', 'Attorney', 'Pittsburgh', 52, 420000],
-      ['Kris Humphrey', 'Agency Legal Counsel', 'Laredo', 30, 150000],
-      ['Frankie Long', 'Industrial Analyst', 'Austin', 31, 170000],
-      ['Brynn Robbins', 'Business Analyst', 'Norfolk', 22, 90000],
-      ['Justice Mann', 'Business Consultant', 'Chicago', 24, 133000],
-      ['Addison Navarro', 'Business Management Analyst', 'New York', 50, 295000],
-      ['Jesse Welch', 'Agency Legal Counsel', 'Seattle', 28, 200000],
-      ['Eli Mejia', 'Commercial Specialist', 'Long Beach', 65, 400000],
-      ['Gene Leblanc', 'Industrial Analyst', 'Hartford', 34, 110000],
-      ['Danny Leon', 'Computer Scientist', 'Newark', 60, 220000],
-      ['Lane Lee', 'Corporate Counselor', 'Cincinnati', 52, 180000],
-      ['Jesse Hall', 'Business Analyst', 'Baltimore', 44, 99000],
-      ['Danni Hudson', 'Agency Legal Counsel', 'Tampa', 37, 90000],
-      ['Terry Macdonald', 'Commercial Specialist', 'Miami', 39, 140000],
-      ['Justice Mccarthy', 'Attorney', 'Tucson', 26, 330000],
-      ['Silver Carey', 'Computer Scientist', 'Memphis', 47, 250000],
-      ['Franky Miles', 'Industrial Analyst', 'Buffalo', 49, 190000],
-      ['Glen Nixon', 'Corporate Counselor', 'Arlington', 44, 80000],
-      ['Gabby Strickland', 'Business Process Consultant', 'Scottsdale', 26, 45000],
-      ['Mason Ray', 'Computer Scientist', 'San Francisco', 39, 142000],
-    ];
 
     const options = {
-      filter: true,
+      filter: false,
       selectableRows: true,
-      filterType: 'dropdown',
+      filterType: 'textField',
       responsive: 'stacked',
       rowsPerPage: 10,
       page: 1,
     };
 
-    return <MUIDataTable title={'ACME Employee list'} data={data} columns={columns} options={options} />;
+    return <MUIDataTable title={'ACME Employee list'} data={DATA} columns={columns} options={options} />;
   }
 }
 
-const theme = createMuiTheme({
-  palette: { type: 'dark' },
-  typography: { useNextVariants: true },
-});
+class Root extends React.Component {
+  state = {
+    themeType: 'light',
+  };
 
-const element = (
-  <MuiThemeProvider theme={theme}>
-    <Example />
-  </MuiThemeProvider>
-);
+  lightTheme = createMuiTheme({
+    typography: { useNextVariants: true },
+    palette: { type: 'light' },
+  });
 
-ReactDOM.render(element, document.getElementById('app-root'));
+  darkTheme = createMuiTheme({
+    typography: { useNextVariants: true },
+    palette: { type: 'dark' },
+  });
+
+  toggleTheme = () => {
+    let newTheme;
+    if (this.state.themeType === 'light') {
+      newTheme = 'dark';
+    } else {
+      newTheme = 'light';
+    }
+    this.setState({ themeType: newTheme });
+  };
+
+  renderToggleButton = () => {
+    const { themeType } = this.state;
+    const isChecked = themeType === 'dark';
+    return (
+      <Paper style={{ marginBottom: '10px' }}>
+        <Switch value="checked" checked={isChecked} onChange={this.toggleTheme} />
+        <Typography variant="body1">Toggle Theme</Typography>
+      </Paper>
+    );
+  };
+
+  render() {
+    let theme;
+    if (this.state.themeType === 'light') {
+      theme = this.lightTheme;
+    } else {
+      theme = this.darkTheme;
+    }
+
+    return (
+      <MuiThemeProvider theme={theme}>
+        {this.renderToggleButton()}
+        <Example />
+      </MuiThemeProvider>
+    );
+  }
+}
+
+ReactDOM.render(<Root />, document.getElementById('app-root'));
